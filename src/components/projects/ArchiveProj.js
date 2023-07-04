b34c004f51b2365fde0244f8d7e8932521c0606a
LudiJolo <joloh18@yahoo.com>
2023-07-04 14:31:57 -0700

    update archive projects
    - update table alignment
    - added github and demo button

--- src/components/projects/ArchiveProj.js
+++ src/components/projects/ArchiveProj.js
@@ -1,27 +1,41 @@
 import React from "react";
-import { Container, Table } from "react-bootstrap";
-
+import { Container, Row, Table, Col, Button } from "react-bootstrap";
+import "./archiveproj.css";
+import { Github } from "react-bootstrap-icons";
 const ArchiveProj = (props) => {
   return (
-    <Container>
-      <Table striped hover variant="dark">
-        <thead>
-          <tr>
-            <th>Semester</th>
-            <th>Beginners</th>
-            <th>Advanced</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.archive.map((sem) => (
-            <tr>
-              <td>{sem.semester}</td>
-              <td>{sem.level.advanced.title}</td>
-              <td>{sem.level.beginners.title}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+    <Container className="py-5 ">
+      <Row id="archive-row" className="justify-content-center">
+        <h3 class="col-12" align="center">Projects from past semesters:</h3>
+        <Col md={8}>
+          <Table striped hover variant="dark">
+            <thead>
+              <tr>
+                <th>Semester</th>
+                <th>Beginners</th>
+                <th>Advanced</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.archive.map((sem) => (
+                <tr>
+                  <td>{sem.semester}</td>
+                  <td>
+                    <Github className="mr-3" />
+                    <span>{sem.level.beginners.title}</span>
+                    <Button variant="success">Play demo</Button>
+                  </td>
+                  <td>
+                    <Github className="mr-3" />
+                    <span>{sem.level.advanced.title}</span>
+                    <Button variant="success">Play demo</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
     </Container>
   );
 };
